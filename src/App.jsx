28ce7455d6c73bb17f4f39e97a39ec5050d0d05a
Vinaymandloi448 <vinay.mandloi.averybit@gmail.com>
2026-08-20 18:35:27 +0530

    feat: initialize React project with Vite, Tailwind CSS, and base project structure

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-950">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold text-white">
+          FacultyConnect
+        </h1>
+
+        <p className="mt-3 text-slate-400">
+          Faculty Communication & Collaboration Platform
+        </p>
+
+        <button className="mt-6 rounded-lg bg-blue-600 px-5 py-3 font-medium text-white hover:bg-blue-700">
+          Tailwind Working
+        </button>
+      </div>
+    </div>
+  )
+}
+
+export default App
